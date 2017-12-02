587812e1e88e1d5a3bea45d2fecd44920a10cd7e
Sanja-Vesic <sanja.vesic@hotmail.rs>
2017-12-02 15:02:11 +0100

    finished

--- js/Pomodorist.js
+++ js/Pomodorist.js
@@ -1,22 +1,71 @@
-document.getElementById('timer').innerHTML =
-  03 + ":" + 00;
-startTimer();
+var interval = undefined;
+var inverval05 = undefined;
+var sound = new Audio();
+sound.src = "sound/yay.wav";
 
-function startTimer() {
-  var presentTime = document.getElementById('timer').innerHTML;
-  var timeArray = presentTime.split(/[:]+/);
-  var m = timeArray[0];
-  var s = checkSecond((timeArray[1] - 1));
-  if(s==59){m=m-1}
-  //if(m<0){alert('timer completed')}
-  
-  document.getElementById('timer').innerHTML =
-    m + ":" + s;
-  setTimeout(startTimer, 1000);
+function start() {
+    var timer = document.getElementById("mytimer").innerHTML;
+    var arr = timer.split(":");
+    var min = arr[0];
+    var sec = arr[1];
+
+    if (sec == 0) {
+        if (min == 0) {
+            sound.play();
+            return;
+        }
+        min--;
+        sec = 60;
+        if (min < 10) min = "0" + min;
+        sec = 59;
+    } else sec--;
+    if (sec < 10) sec = "0" + sec;
+    document.getElementById("mytimer").innerHTML = min + ":" + sec;
+    interval = setTimeout(start, 1000);
 }
 
-function checkSecond(sec) {
-  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
-  if (sec < 0) {sec = "59"};
-  return sec;
+//------stop main timer-----
+function stop() {
+    clearInterval(interval);
+    interval = undefined;
+}
+
+//-----rest main timer-----
+function reset() {
+    clearInterval(interval);
+    document.getElementById("mytimer").innerHTML = '25:00';
+}
+
+
+//-----start break timer-----
+function start05() {
+    var timer = document.getElementById("timer05").innerHTML;
+    var arr = timer.split(":");
+    var min = arr[0];
+    var sec = arr[1];
+    if (sec == 0) {
+        if (min == 0) {
+            sound.play();
+            return;
+        }
+        min--;
+        sec = 60;
+        if (min < 10) min = "0" + min;
+        sec = 59;
+    } else sec--;
+    if (sec < 10) sec = "0" + sec;
+
+    document.getElementById("timer05").innerHTML = min + ":" + sec;
+    interval05 = setTimeout(start05, 1000);
+}
+
+//-----stop break timer-----
+function stop05() {
+    clearInterval(interval05);
+    interval05 = undefined;
+}
+//-----reset break timer-----
+function reset05() {
+    clearInterval(interval05);
+    document.getElementById("timer05").innerHTML = "05:00";
 }
